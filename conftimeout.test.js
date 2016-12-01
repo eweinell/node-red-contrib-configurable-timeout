d0d1d5e0db87f8a48a15f7bb9790eb89513bdb84
Erhard Weinell <erhard@weinell.local>
2016-12-01 22:51:57 +0100

    prep testcase

--- conftimeout.test.js
+++ conftimeout.test.js
@@ -22,13 +22,54 @@ THE SOFTWARE.
 */
 
 'use strict';
-var _ = required("lodash");
+var _ = require("lodash");
 var expect = require("chai").expect;
 
 var tested = require("./conftimeout.js");
 
 describe("ConfigurableTimeoutModule", function() {
   it("loadmodule should work", function() {
-    
+    mockred({}, tested);
   });
 });
+
+function mockred(config, testcase) {
+  var _events = [], _status = undefined, _error = undefined, _messages = [];
+  var RED = {
+      nodes: {
+          registerType: function (nodeName, nodeConfigFunc) {
+              this.nodeConfigFunc = nodeConfigFunc;
+          },
+          createNode: function () {
+          }
+      },
+      on: function (event, eventFunc) {
+          _events[event] = eventFunc;
+      },
+      emit: function (event, data) {
+          _events[event](data);
+      },
+      error: function (error) {
+          if (error) _error = error;
+          return _error;
+      },
+      status: function (status) {
+          if (status) _status = status;
+          return _status;
+      },
+      log: function () {
+          console.log.apply(this, arguments);
+      },
+      send: function (msg) {
+          assert(msg);
+          _messages.push(msg);
+      },
+      messages: function (messages) {
+          if (messages) _messages = messages;
+          return _messages;
+      }
+  };
+  testcase(RED);
+  RED.nodes.nodeConfigFunc.call(RED, config);
+  return RED;
+}
